8ad08a9631d137cf0713f39161e06e92b5cc9922
Matías Nicolás Sosa <mnsosa.ms@gmail.com>
2025-06-20 09:00:38 -0300

    Update detalles como nombre pestaña

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Flor <3',
+  description: 'Portfolio y blog de Florencia',
 }
 
 export default function RootLayout({
